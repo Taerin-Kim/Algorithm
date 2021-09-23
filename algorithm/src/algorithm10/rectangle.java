8165f7e228e5ec6949b3963b494e8f351bd0a244
Blaire <76199258+Taerin-Kim@users.noreply.github.com>
2021-09-23 18:04:22 +0900

    n*n 사각형 출력

--- algorithm/src/algorithm10/rectangle.java
+++ algorithm/src/algorithm10/rectangle.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class rectangle {
 
 	public static void main(String[] args) {
-		
+
 //		입력된 수(N) 만큼 N행 N열의 형태로 연속으로 출력되는 
 //		   숫자 사각형을 출력하시오
 //		4
@@ -15,16 +15,19 @@ public class rectangle {
 //		       9 10 11 12
 //		       13 14 15 16 
 //		중첩 반복문 (for 혹은 while)
-		
+
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		
-		//n행부터 출력
-		for(int i = 0; i < n; i++ ) {
-			
-			
+
+		// n행부터 출력 i는 숫자
+		for (int i = 1; i <= n * n; i++) {
+			// j는 줄 숫자
+			if (i % n == 0) { // n의 배수이면 n개까지출력 되고 줄바뀜
+				System.out.println(i);
+			} else { // n의 배수가 아니면 그대로 같은 줄에 출력
+				System.out.printf("%s ", i);
+			}
 		}
-		
 	}
 
 }
