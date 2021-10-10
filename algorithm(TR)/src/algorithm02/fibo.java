f4e22dcb02a413eb2eddee6f9450521c627b4f95
Blaire <76199258+Taerin-Kim@users.noreply.github.com>
2021-10-10 21:46:34 +0900

    피보나치수열

--- algorithm(TR)/src/algorithm02/fibo.java
+++ algorithm(TR)/src/algorithm02/fibo.java
@@ -3,8 +3,19 @@ package algorithm02;
 public class fibo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 문제 : 피보나치 수열을 입력 받은 숫자 개수만큼 출력하라
 
+		int input = 8; // 8개 출력
+		for (int i = 1; i <= input; i++) {
+			System.out.println(fibo(i));
+		}
 	}
 
+	public static int fibo(int n) {
+		if (n <= 1) {
+			return n;
+		} else {
+			return fibo(n - 2) + fibo(n - 1);
+		}
+	}
 }
